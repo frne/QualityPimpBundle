fb196d8d40d32e31cecf61d3dfd99dfd8687e414
frne <fneff89@gmail.com>
2015-01-11 13:36:40 +0100

    Initial configuration

---
+++ QualityPimpBundle.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @project frne/quality-pimp-bundle
+ * @license MIT
+ * @author Frank Neff
+ */
+
+namespace Frne\Bundle\Neo4jUserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class QualityPimpBundle extends Bundle
+{
+}
